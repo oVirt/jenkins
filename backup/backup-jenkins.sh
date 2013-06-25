142b96f568d0cbe45ed7a95fab1f8cf5bffd0eb3
Kiril Nesenko <knesenko@redhat.com>
2013-06-25 11:44:03 +0300

    Backup plugins directory only on Sunday
    Change-Id: I685c026a7b9d882297aa43c84527d1a7a774b210
    Signed-off-by: Kiril Nesenko <knesenko@redhat.com>

--- backup/backup-jenkins.sh
+++ backup/backup-jenkins.sh
@@ -3,6 +3,11 @@
 JENKINS_DIR=/var/lib/jenkins
 BACKUP_DIR=/backups
 DAILY_BACKUPS_TO_KEEP=30
+BACKUP_PLUGINS=
+
+if [[ $(date +%a) =~ Sun ]]; then
+    BACKUP_PLUGINS=plugins
+fi
 
 if [[ ! -d "${BACKUP_DIR}" ]]; then
     mkdir -p "${BACKUP_DIR}" \
@@ -17,7 +22,7 @@ pushd "${JENKINS_DIR}"
 find -maxdepth 1 -type f -name "*.xml" > "${JENBAKLISTFILE}"
 find users \
      userContent \
-     plugins >> "${JENBAKLISTFILE}"
+     ${BACKUP_PLUGINS} >> "${JENBAKLISTFILE}"
 find -H jobs -maxdepth 2 -name "config.xml" -type f >> "${JENBAKLISTFILE}"
 tar czvf "${BACKUP_DIR}"/jenkins-backup-$(date +"%Y-%m-%d_%H_%M_%S").tgz -T "${JENBAKLISTFILE}"
 popd
