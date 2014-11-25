45636852241fd619191ccbe79cae01ddf24a331b
David Caro <dcaroest@redhat.com>
2014-11-25 13:19:33 +0100

    Added extra mock cache cleanup
    It's getting too heavy to keep, and we are going to setup proxies to do the
    caching instead.
    
    Change-Id: I4666d6f163be454e9131721c2bce900e7eaebe24
    Signed-off-by: David Caro <dcaroest@redhat.com>

--- jobs/confs/shell-scripts/mock_cleanup.sh
+++ jobs/confs/shell-scripts/mock_cleanup.sh
@@ -2,6 +2,9 @@
 echo "shell-scripts/mock_cleanup.sh"
 # remove chroot to free space
 sudo rm -Rf mock mock-cache
+# remove mock system cache, we will setup proxies to do the caching and this
+# takes lots of space between runs
+sudo rm -Rf /var/cache/mock/*
 # compress logs
 pushd "$WORKSPACE"/exported-artifacts
 shopt -s nullglob
