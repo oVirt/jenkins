f2544a4590c77184f8bb89ca6f3b580f74d99154
David Caro <dcaroest@redhat.com>
2014-11-26 13:01:19 +0100

    Doing proper mock cleanup
    Now it checks if there are any mounted dirs inside the chroot and tries to
    umount before removing. It will fail if unable to umount.
    
    Also the cleanup has been put as post-build task, so it should always run.
    Changed the logs tarball extension to more standard tgz
    
    Change-Id: I20cb54fb968556f3d1bdcf3a9ab14e826c56b352
    Signed-off-by: David Caro <dcaroest@redhat.com>

--- jobs/confs/shell-scripts/mock_cleanup.sh
+++ jobs/confs/shell-scripts/mock_cleanup.sh
@@ -1,12 +1,70 @@
 #!/bin/bash -xe
 echo "shell-scripts/mock_cleanup.sh"
-# remove chroot to free space
+
+shopt -s nullglob
+
+# Archive the logs, we want them anyway
+pushd "$WORKSPACE"/exported-artifacts
+logs=(*log *_pkgs "$WORKSPACE"/*log)
+if [[ "$logs" ]]; then
+    tar cvzf logs.tgz "${logs[@]}"
+    rm -f *log *_pkgs
+fi
+popd
+
+failed=false
+
+mock_dir="$WORKSPACE/mock"
+# stop any processes running inside the chroot
+chroots=("$WORKSPACE"/mock/*)
+for chroot in "${chroots[@]}"; do
+    echo "Cleaning up chroot $chroot"
+    ### Generate the mock configuration
+    pushd "$WORKSPACE"/jenkins/mock_configs
+    mock_conf="${chroot##*/}"
+    base_conf="${mock_conf%%x86_64*}x86_64"
+    echo "#### Generating mock configuration"
+    ./mock_genconfig \
+        --name="$mock_conf" \
+        --base="$base_conf.cfg" \
+        --option="basedir=$WORKSPACE/mock/" \
+    > "$mock_conf.cfg"
+    cat "$mock_conf.cfg"
+    popd
+
+    my_mock="/usr/bin/mock"
+    my_mock+=" --configdir=$WORKSPACE/jenkins/mock_configs"
+    my_mock+=" --root=$mock_conf"
+
+    #TODO: investigate why mock --clean fails to umount certain dirs sometimes,
+    #so we can use it instead of manually doing all this.
+    echo "Killing all mock orphan processes, if any."
+    $my_mock \
+        --orphanskill \
+    || {
+        echo "ERROR:  Failed to kill orphans on $chroot."
+        failed=true
+    }
+
+    mounts=($(mount | awk '{print $3}' | grep "$chroot")) || :
+    if [[ "$mounts" ]]; then
+        echo "Found mounted dirs inside the chroot $chroot. Trying to umount."
+    fi
+    for mount in "${mounts[@]}"; do
+        sudo umount "$mount" \
+        || {
+            echo "ERROR:  Failed to umount $mount."
+            failed=true
+        }
+    done
+done
+
+if $failed; then
+    echo "Aborting."
+    exit 1
+fi
 sudo rm -Rf mock mock-cache
+
 # remove mock system cache, we will setup proxies to do the caching and this
 # takes lots of space between runs
 sudo rm -Rf /var/cache/mock/*
-# compress logs
-pushd "$WORKSPACE"/exported-artifacts
-shopt -s nullglob
-tar cvjf logs.tbz *log *_pkgs "$WORKSPACE"/*log
-rm -f *log *_pkgs
--- jobs/confs/shell-scripts/mock_setup.sh
+++ jobs/confs/shell-scripts/mock_setup.sh
@@ -1,10 +1,67 @@
 #!/bin/bash -xe
 echo "shell-scripts/mock_setup.sh"
+
+shopt -s nullglob
+
 # cleanup and setup env
 if [[  "$CLEAN_CACHE" == "true" ]]; then
     sudo rm -Rf /var/cache/mock
 fi
-sudo rm -Rf mock mock-cache exported-artifacts
+
+failed=false
+
+mock_dir="$WORKSPACE/mock"
+# stop any processes running inside the chroot
+chroots=("$WORKSPACE"/mock/*)
+for chroot in "${{chroots[@]}}"; do
+    echo "Cleaning up chroot $chroot"
+    ### Generate the mock configuration
+    pushd "$WORKSPACE"/jenkins/mock_configs
+    mock_conf="${{chroot##*/}}"
+    base_conf="${{mock_conf%%x86_64*}}x86_64"
+    echo "#### Generating mock configuration"
+    ./mock_genconfig \
+        --name="$mock_conf" \
+        --base="$base_conf.cfg" \
+        --option="basedir=$WORKSPACE/mock/" \
+    > "$mock_conf.cfg"
+    cat "$mock_conf.cfg"
+    popd
+
+    my_mock="/usr/bin/mock"
+    my_mock+=" --configdir=$WORKSPACE/jenkins/mock_configs"
+    my_mock+=" --root=$mock_conf"
+
+    echo "Killing all mock orphan processes, if any."
+    $my_mock \
+        --orphanskill \
+    || {{
+        echo "ERROR:  Failed to kill orphans on $chroot."
+        failed=true
+    }}
+
+    mounts=($(mount | awk '{{print $3}}' | grep "$chroot")) || :
+    if [[ "$mounts" ]]; then
+        echo "Found mounted dirs inside the chroot $chroot. Trying to umount."
+    fi
+    for mount in "${{mounts[@]}}"; do
+        sudo umount "$mount" \
+        || {{
+            echo "ERROR:  Failed to umount $mount."
+            failed=true
+        }}
+    done
+done
+
+# If we failed in any step, abort to avoid breaking the host
+if $failed; then
+    echo "Aborting."
+    exit 1
+fi
+
+sudo rm -Rf mock mock-cache
+
+sudo rm -Rf mock-cache exported-artifacts
 mkdir -p mock exported-artifacts
 chgrp mock mock "$WORKSPACE" "$WORKSPACE"/exported-artifacts
 chmod g+rws mock
