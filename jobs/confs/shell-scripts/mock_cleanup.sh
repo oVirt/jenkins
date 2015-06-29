d482908b3946625a703cb2818fcf30b4c8198f55
David Caro <dcaroest@redhat.com>
2015-06-29 19:21:23 +0200

    Using mock_runner to run the mock standard jobs
    That will allow better reproducibility and will allow devels to run on their
    laptops before sending
    
    Change-Id: I48425d1985a3dc84f8850522d7e9a9ebf1d39162
    Signed-off-by: David Caro <dcaroest@redhat.com>

--- jobs/confs/shell-scripts/mock_cleanup.sh
+++ jobs/confs/shell-scripts/mock_cleanup.sh
@@ -4,6 +4,8 @@ echo "shell-scripts/mock_cleanup.sh"
 shopt -s nullglob
 
 
+WORKSPACE="$PWD"
+
 # Make clear this is the cleanup, helps reading the jenkins logs
 cat <<EOC
 _______________________________________________________________________
@@ -16,37 +18,26 @@ EOC
 
 
 # Archive the logs, we want them anyway
-pushd "$WORKSPACE"/exported-artifacts
-logs=(*log *_pkgs "$WORKSPACE"/*log)
+logs=(
+    ./*log
+    ./*/logs
+)
 if [[ "$logs" ]]; then
-    tar cvzf logs.tgz "${logs[@]}"
-    rm -f *log *_pkgs
+    tar cvzf exported-artifacts/logs.tgz "${logs[@]}"
+    rm -rf "${logs[@]}"
 fi
-popd
 
-failed=false
-
-mock_dir="$WORKSPACE/mock"
 # stop any processes running inside the chroot
-chroots=("$WORKSPACE"/mock/*)
-for chroot in "${chroots[@]}"; do
-    echo "Cleaning up chroot $chroot"
-    ### Generate the mock configuration
-    pushd "$WORKSPACE"/jenkins/mock_configs
-    mock_conf="${chroot##*/}"
-    base_conf="${mock_conf%%x86_64*}x86_64"
-    echo "#### Generating mock configuration"
-    ./mock_genconfig \
-        --name="$mock_conf" \
-        --base="$base_conf.cfg" \
-        --option="basedir=$WORKSPACE/mock/" \
-    > "$mock_conf.cfg"
-    cat "$mock_conf.cfg"
-    popd
-
+failed=false
+mock_confs=("$WORKSPACE"/*/mocker*)
+for mock_conf_file in "${mock_confs[@]}"; do
+    [[ "$mock_conf_file" ]] || continue
+    echo "Cleaning up mock $mock_conf"
+    mock_root="${mock_conf_file##*/}"
+    mock_root="${mock_root%.*}"
     my_mock="/usr/bin/mock"
-    my_mock+=" --configdir=$WORKSPACE/jenkins/mock_configs"
-    my_mock+=" --root=$mock_conf"
+    my_mock+=" --configdir=${mock_conf_file%/*}"
+    my_mock+=" --root=${mock_root}"
     my_mock+=" --resultdir=$WORKSPACE"
 
     #TODO: investigate why mock --clean fails to umount certain dirs sometimes,
@@ -59,7 +50,13 @@ for chroot in "${chroots[@]}"; do
         failed=true
     }
 
-    mounts=($(mount | awk '{print $3}' | grep "$chroot")) || :
+    mock_root="$(\
+        grep \
+            -Po "(?=<config_opts\['root'\] = ')[^']*" \
+            "$mock_conf_file" \
+    )" || :
+    [[ "$mock_root" ]] || continue
+    mounts=($(mount | awk '{print $3}' | grep "$mock_root")) || :
     if [[ "$mounts" ]]; then
         echo "Found mounted dirs inside the chroot $chroot. Trying to umount."
     fi
@@ -76,7 +73,6 @@ if $failed; then
     echo "Aborting."
     exit 1
 fi
-sudo rm -Rf mock mock-cache
 
 # remove mock system cache, we will setup proxies to do the caching and this
 # takes lots of space between runs
--- jobs/confs/shell-scripts/standard_check_patch.sh
+++ jobs/confs/shell-scripts/standard_check_patch.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -xe
+#!/bin/bash -e
 echo "shell-scripts/standard_check_patch.sh"
 # PARAMETERS
 #
@@ -12,149 +12,23 @@ echo "shell-scripts/standard_check_patch.sh"
 #
 # arch
 #     Architecture to build the packages for
-#
-# extra-packages
-#     List of packages to install, should be empty as it should use the .req
-#     files instead
-#
-# extra-repos
-#     List of extra repositories to use when building, in a space separated list
-#     of name,url pairs
-#
-# extra-env
-#     Extra environment variables
 
 distro="{distro}"
 arch="{arch}"
 project="{project}"
-extra_packages=({extra-packages})
-extra_repos=({extra-repos})
-extra_env=({extra-env})
-WORKSPACE=$PWD
-script="automation/check-patch.sh"
+WORKSPACE="$PWD"
 
-
-### Generate the mock configuration
-pushd "$WORKSPACE"/jenkins/mock_configs
-case $distro in
-    fc*) distribution="fedora-${{distro#fc}}";;
-    el*) distribution="epel-${{distro#el}}";;
-    *) echo "Unknown distro $distro"; exit 1;;
-esac
-mock_conf="${{distribution}}-$arch-ovirt-snapshot"
-mock_repos=()
-for mock_repo in "${{extra_repos[@]}}"; do
-    mock_repos+=("--repo" "$mock_repo")
-done
-echo "#### Generating mock configuration"
-./mock_genconfig \
-    --name="$mock_conf" \
-    --base="$distribution-$arch.cfg" \
+cd "./$project"
+"$WORKSPACE"/jenkins/mock_configs/mock_runner.sh \
+    --patch-only \
+    --mock-confs-dir "$WORKSPACE"/jenkins/mock_configs \
     --try-proxy \
-    --option="basedir=$WORKSPACE/mock/" \
-    "${{mock_repos[@]}}" \
-> "$mock_conf.cfg"
-sudo touch /var/cache/mock/*/root_cache/cache.tar.gz &>/dev/null || :
-cat "$mock_conf.cfg"
-popd
+    "$distro.*$arch"
 
-## prepare the command line
-my_mock="/usr/bin/mock"
-my_mock+=" --configdir=$WORKSPACE/jenkins/mock_configs"
-my_mock+=" --root=$mock_conf"
-my_mock+=" --resultdir=$WORKSPACE/exported-artifacts"
-
-## init the chroot
-$my_mock \
-    --init
-
-### Configure extra yum vars
-echo "Configuring custom env variables for repo urls"
-$my_mock \
-    --no-clean \
-    --shell <<EOF
-        mkdir -p /etc/yum/vars
-        echo "$distro" > /etc/yum/vars/distro
-EOF
-
-### Install any extra packages if needed
-## from config
-if [[ -n "$extra_packages" ]]; then
-    echo "##### Installing extra dependencies: $extra_packages"
-    $my_mock \
-        --no-clean \
-        --install "${{extra_packages[@]}}"
-fi
-## from project requirements file
-build_deps_file="$project/automation/check-patch.req.${{distro}}"
-[[ -f "$build_deps_file" ]] \
-|| build_deps_file="$project/automation/check-patch.req"
-if [[ -f  "$build_deps_file" ]]; then
-    packages=($(cat "$build_deps_file"))
-    if [[ -n "${{packages}}" ]]; then
-        echo "##### Installing extra dependencies from $build_deps_file"
-        $my_mock \
-            --no-clean \
-            --install "${{packages[@]}}"
-    else
-        echo "#### WARN: empty req file found $build_deps_file"
-    fi
-fi
-
-## Needed when running yum inside the chroot on different distro than the host,
-## because rpmdb is first generated with the host yum when creating the chroot
-## and sometimes is not compatible with the chroot installed yum version
-$my_mock \
-    --no-clean \
-    --shell <<EOF
-rm -f /var/lib/rpm/__db*
-rpm --rebuilddb
-EOF
-
-### Mount the current workspace in the chroot
-$my_mock \
-    --no-clean \
-    --shell <<EOFMAKINGTHISHARDTOMATCH
-mkdir -p /tmp/run
-EOFMAKINGTHISHARDTOMATCH
-
-# Adding the workspace mount to the config
-pushd "$WORKSPACE"/jenkins/mock_configs
-echo "#### Generating mock configuration with workspace mounted"
-./mock_genconfig \
-    --name="$mock_conf" \
-    --base="$distribution-$arch.cfg" \
-    --try-proxy \
-    --option="basedir=$WORKSPACE/mock/" \
-    --option="resultdir=$WORKSPACE/exported-artifacts" \
-    --option="plugin_conf.bind_mount_enable=True" \
-    --option='plugin_conf.bind_mount_opts.dirs=[
-        ["'"$WORKSPACE"'", "/tmp/run"]
-    ]' \
-    "${{mock_repos[@]}}" \
-> "$mock_conf.cfg"
-sudo touch /var/cache/mock/*/root_cache/cache.tar.gz &>/dev/null || :
-cat "$mock_conf.cfg"
-popd
-
-### Run the script
-echo "##### Running $SCRIPT inside mock"
-$my_mock \
-    --no-clean \
-    --shell <<EOFMAKINGTHISHARDTOMATCH
-set -e
-SCRIPT="/tmp/run/$project/$script"
-export HOME=/tmp/run
-cd \$HOME/$project
-chmod +x \$SCRIPT
-\$SCRIPT
-echo -e "\n\n"
-[[ -d exported-artifacts  ]] && ls -la exported-artifacts
-echo "Finished running the script"
-EOFMAKINGTHISHARDTOMATCH
-
+# move the exported artifacts to jenkins workspace, as they are created in the
+# project root
 mkdir -p "$WORKSPACE"/exported-artifacts
-sudo chown -R $USER:$USER "$WORKSPACE/exported-artifacts"
+sudo chown -R "$USER:$USER" "$WORKSPACE/exported-artifacts"
 if ls "$WORKSPACE/$project/exported-artifacts/"* &>/dev/null; then
     sudo mv "$WORKSPACE/$project/exported-artifacts/"* \
             "$WORKSPACE/exported-artifacts/"
