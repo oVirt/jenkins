388706064f2264896b0071977433d5d05eb00d3b
David Caro <dcaroest@redhat.com>
2015-11-20 20:35:44 +0100

    Adding first pipeline
    Using workflow, for now, only for lago. The pipeline is triggered only
    on merge for now and ends deploying the generated artifacts to the
    lago-stapshot repo if the checks and builds passed for all the
    distros.
    
    Requires ovirt custom jjb>=1.2.1.dev43-1 (available in the ci-tools
    repo)
    
    This will create one snapshot repo for each pipeline run, suffixed
    with the build tag (jenkins-${JOB_NAME}-${BUILD_NUMBER}) of the
    pipeline
    
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1277650
    Change-Id: Ie5d510ef6388c4cf178c31971dc05941a4a19d0a
    Signed-off-by: David Caro <dcaroest@redhat.com>

---
+++ jobs/confs/groovy-scripts/standard-pipeline.groovy
@@ -0,0 +1,143 @@
+import jenkins.model.Jenkins
+
+project='{project}'
+branch='{branch}'
+trigger_stage='{stage}'
+job_prefix='http://jenkins.ovirt.org/job/'
+with_build_and_deploy = false
+
+
+if (trigger_stage == 'check-merged') {{
+    with_build_and_deploy = true
+}}
+
+
+// This decorator allows using jenkins internal non-serializable objects
+// see https://github.com/jenkinsci/workflow-plugin/blob/master/TUTORIAL.md
+@NonCPS
+def get_jobs(project, version, trigger_stage) {{
+    def jobs = Jenkins.instance.items.findAll {{ job ->
+        job.name =~ /^${{project}}_${{version}}_${{trigger_stage}}/ \
+        && job.name != env.JOB_NAME
+    }}.collect {{ job ->
+        job.name
+    }}
+    return jobs
+}}
+
+
+def run_checks(project, branch, trigger_stage) {{
+    // runs the check jobs
+    stage concurrency: 5, name: 'Running checks'
+    def check_jobs = get_jobs(project, branch, trigger_stage)
+    println "Found ${{check_jobs.size()}} check jobs"
+    def checkers = [:]
+    for (int i = 0; i < check_jobs.size(); i++) {{
+        def job_name = check_jobs[i]
+        checkers[job_name] = {{
+            build([
+                job: job_name,
+                parameters: [
+                    [
+                        $class: 'StringParameterValue',
+                        name: 'GERRIT_REFSPEC',
+                        value: GERRIT_REFSPEC
+                    ],
+                    [
+                        $class: 'StringParameterValue',
+                        name: 'GERRIT_BRANCH',
+                        value: GERRIT_BRANCH
+                    ]
+                ]
+            ])
+        }}
+    }}
+    return parallel(checkers)
+}}
+
+
+def build_artifacts(project, branch) {{
+    // runs the build jobs
+    stage concurrency: 5, name: 'Building'
+    def build_jobs = get_jobs(project, branch, 'build-artifacts')
+    println "Found ${{build_jobs.size()}} build jobs"
+    def builders = [:]
+    for (int i = 0; i < build_jobs.size(); i++) {{
+        def job_name = build_jobs[i]
+        builders[job_name] = {{
+            build([
+                job: job_name,
+                parameters: [
+                    [
+                        $class: 'StringParameterValue',
+                        name: 'GERRIT_REFSPEC',
+                        value: GERRIT_REFSPEC
+                    ],
+                    [
+                        $class: 'StringParameterValue',
+                        name: 'GERRIT_BRANCH',
+                        value: GERRIT_BRANCH
+                    ]
+                ]
+            ])
+        }}
+    }}
+    return parallel(builders)
+}}
+
+
+@NonCPS
+def extract_builders_list(build_results) {{
+    // this is not serializable, thus it has to go in a nonCPS method
+    // retrieve all the full urls of the builders, to pass to the deplayers
+    def builders_list = []
+    build_results.each {{ job ->
+        builders_list.add(
+            job.getValue().getAbsoluteUrl()
+        )
+    }}
+    return builders_list
+}}
+
+
+def deploy(build_results, project, branch) {{
+    // Runs the deploy jobs with the build_results
+    builders_list = extract_builders_list(build_results)
+    stage concurrency: 1, name: 'Deploying to snapshot'
+    def deploy_jobs = get_jobs(project, branch, 'deploy-to-.*-snapshot')
+    println "Found ${{deploy_jobs.size()}} deploy jobs"
+    def deployers = [:]
+    for (int i = 0; i < deploy_jobs.size(); i++) {{
+        def job_name = deploy_jobs[i]
+        deployers[job_name] = {{
+            build([
+                job: job_name,
+                parameters: [
+                    [
+                        $class: 'StringParameterValue',
+                        name: 'BUILD_ARTIFACTS_JOBS',
+                        value: builders_list.join(',')
+                    ],
+                    [
+                        $class: 'StringParameterValue',
+                        name: 'BUILD_ID',
+                        value: "${{env.JOB_NAME}}-${{env.BUILD_NUMBER}}"
+                    ]
+                ]
+            ])
+        }}
+    }}
+    return parallel(deployers)
+}}
+
+
+def main() {{
+    def check_results = run_checks(project, branch, trigger_stage)
+    if (with_build_and_deploy) {{
+        def build_results = build_artifacts(project, branch)
+        def deploy_results = deploy(build_results, project, branch)
+    }}
+}}
+
+// dummy useless main
+main()
