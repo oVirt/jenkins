236268c34a07a9019e930a23abcc8bf0f85c7c7d
David Caro <dcaroest@redhat.com>
2016-03-19 17:58:03 +0100

    Remove all the log gathering from the system job
    Now it's the runner script that will gather it as it belongs along the
    code
    
    Change-Id: I6a1047eab78ca2dd6067410606725368f1bf62ba
    Signed-off-by: David Caro <dcaroest@redhat.com>

--- jobs/confs/shell-scripts/system_tests.collect_logs.sh
+++ jobs/confs/shell-scripts/system_tests.collect_logs.sh
@@ -9,28 +9,11 @@ VERSION={version}
 
 WORKSPACE="$PWD"
 OVIRT_SUITE="basic_suite_$VERSION"
-PREFIX="$WORKSPACE/ovirt-system-tests/deployment-$OVIRT_SUITE/current"
+TESTS_LOGS="$WORKSPACE/ovirt-system-tests/exported-artifacts"
 
+rm -rf "$WORKSPACE/exported-artifacts"
 mkdir -p "$WORKSPACE/exported-artifacts"
 
-if [[ -d "$PREFIX" ]]; then
-
-    if [[ -d "$PREFIX/test_logs/" ]]; then
-        cp -av \
-            "$PREFIX/test_logs/" \
-            "$WORKSPACE/exported-artifacts/extracted_logs"
-    fi
-
-    if [[ -d "$PREFIX/logs/" ]]; then
-        cp -av \
-            "$PREFIX/logs/" \
-            "$WORKSPACE/exported-artifacts/lago_logs"
-    fi
-
-    find "$PREFIX" \
-        -maxdepth 1 \
-        -iname \*.xml \
-        -exec mv {{}} "$WORKSPACE/exported-artifacts" \;
-
-    rm -rf "$PREFIX"
+if [[ -d "$TESTS_LOGS" ]]; then
+    mv "$TESTS_LOGS/"* "$WORKSPACE/exported-artifacts/"
 fi
