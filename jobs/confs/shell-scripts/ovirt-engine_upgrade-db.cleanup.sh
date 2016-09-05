3bf3154287e703ccc4f58136f0fdb1b9ba009882
Eyal Edri <eedri@redhat.com>
2016-09-05 09:44:50 +0300

    remove ovirt-engine update db job in ci
    	This job is obseleted by the setup-upgrade
    	job which already does DB update internally
    	as part of the upgrade process and does
    	a much more comprehensive test flow
    
    Change-Id: If5a2d79d94d1fd8aece5b32746480cb91f0f153a
    Signed-off-by: Eyal Edri <eedri@redhat.com>

--- jobs/confs/shell-scripts/ovirt-engine_upgrade-db.cleanup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -x
-echo "shell-scripts/ovirt-engine_upgrade-db.cleanup.sh"
-
-DBNAME="${{JOB_NAME//[\/=]/_}}_${{BUILD_NUMBER}}"
-echo "Dropping db  ${{DBNAME}}"
-sudo -u postgres dropdb "$DBNAME"
