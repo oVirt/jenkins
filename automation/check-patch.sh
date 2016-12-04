77872cebbb407103238c62b738a0746da626a4e4
Barak Korren <bkorren@redhat.com>
2016-12-04 22:37:43 +0200

    Add http_proxy env var to proxied mock envs
    This will hopefully allow tools running inside the mock env to take
    advantage of the proxy (Perhaps even with no extra conf needed for some)
    
    Also improved CI for this repo by making it run on more platforms and
    not do unnecessary tests
    
    Change-Id: I91e4b99acc05f78fc2d4cee01348553fa299e01c
    Signed-off-by: Barak Korren <bkorren@redhat.com>

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -1,4 +1,42 @@
 #!/bin/bash -xe
 
-sh -xe automation/jenkins_check_yaml.sh
-python automation/check_publishers_not_deleted.py
+main() {
+    # dispatch tests according to what changed in git
+    local changed_files
+    changed_files="$(git show --pretty="format:" --name-only)"
+
+    if echo "$changed_files" | grep -q '^mock_configs/'; then
+        test_standard_ci "$@"
+    fi
+    if echo "$changed_files" | grep -q '^jobs/'; then
+        # we only run jjb on el7/x86)64 so skip otherwise
+        if is_jjb_test_arch; then
+            test_job_configs "$@"
+        else
+            echo "Skipped JJB testing on this platform"
+        fi
+    fi
+}
+
+is_jjb_test_arch() {
+    [[ "$(uname -m)" == "x86_64" ]] &&
+        grep -qE '^(Red Hat|CentOS) .* release 7\.' /etc/system-release
+}
+
+test_job_configs() {
+    sh -xe automation/jenkins_check_yaml.sh
+    python automation/check_publishers_not_deleted.py
+}
+
+test_standard_ci_proxy() {
+    if [[ -n "$http_proxy" ]]; then
+        echo "It looked like we are running in a PROXIED environment"
+        echo "http_proxy='$http_proxy'"
+    fi
+}
+
+test_standard_ci() {
+    test_standard_ci_proxy
+}
+
+main "$@"
