47ab5164a7c7c645ab5a262d3746df378b03c307
Barak Korren <bkorren@redhat.com>
2017-02-16 10:42:07 +0200

    Integrate whitelist into std-CI
    White-listing patches is espcially important for Standard-CI where any
    patch can change the automation scripts.
    
    Change-Id: Ied5a57460eb1edecaf604ab28e67a946cd591c5a
    Signed-off-by: Barak Korren <bkorren@redhat.com>
    Jira-Ticket: https://ovirt-jira.atlassian.net/browse/OVIRT-1153

--- jobs/confs/shell-scripts/whitelist-filter.sh
+++ jobs/confs/shell-scripts/whitelist-filter.sh
@@ -6,10 +6,10 @@ echo "shell-scripts/whitelist-filter.sh"
 
 # Check whitelist
 if ! [[ -z "$GERRIT_PATCHSET_UPLOADER_EMAIL" ]] \
-    && [[ "@redhat.com" != "${GERRIT_PATCHSET_UPLOADER_EMAIL: -11}" ]] \
     && ! egrep -q \
              -e "^$GERRIT_PATCHSET_UPLOADER_EMAIL\$" \
-             jenkins-whitelist/jenkins-whitelist.txt; then
+             jenkins-whitelist/jenkins-whitelist.txt
+then
    echo "USER $GERRIT_PATCHSET_UPLOADER_EMAIL NOT FOUND IN THE WHITELIST, NOT RUNNING"
    exit 1
 fi
