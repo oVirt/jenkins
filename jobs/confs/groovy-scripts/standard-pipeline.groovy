443085b341aea1beaf810f6def609ff7fece2f98
Nadav Goldin <ngoldin@redhat.com>
2017-02-19 12:38:19 +0200

    lago-pipeline: refactoring
    1. Assert 'sha1' and 'REFSPEC' parameters are passed to the job,
    otherwise fail. This will be done inside a preliminary stage, thus
    allowing to use the proper Jenkins stage failure mechanism, and avoid
    messing with buildResult parameters. Did not include assertion for
    gerrit parameters, as we use this flow for github only at the moment.
    
    2. Use 'params' instead of 'env' where applicable.
    
    3. Use the new 'stage' syntax.
    
    4. Remove code duplication and unify functions.
    
    5. Use the new milestone syntax to guarantee only one job gets to the
    deploy stage.
    
    6. Moved main() to the top of the code.
    
    7. JJB variables will be substituted into the main function call.
    
    Change-Id: I13f3b9ac30266a6372e9b0e34de8511629f9453c
    Signed-off-by: Nadav Goldin <ngoldin@redhat.com>

--- jobs/confs/groovy-scripts/standard-pipeline.groovy
+++ jobs/confs/groovy-scripts/standard-pipeline.groovy
@@ -1,165 +1,96 @@
 import jenkins.model.Jenkins
 
-project='{project}'
-branch='{branch}'
-trigger_stage='{stage}'
-job_prefix='http://jenkins.ovirt.org/job/'
-with_build_and_deploy = false
+// substituted by JJB
+main('{project}', '{branch}', '{stage}')
 
-
-if (trigger_stage == 'check-merged') {{
-    with_build_and_deploy = true
+def main(project, branch, trigger_stage) {{
+    stage("Asserting input") {{
+        def result = check_empty_params(['sha1', 'REFSPEC'])
+        result && error(result)
+    }}
+    stage(trigger_stage) {{
+        run_standard_ci_stage(project, branch, trigger_stage)
+    }}
+    if (trigger_stage == 'check-merged') {{
+        def build_results = null
+        stage('build-artifacts') {{
+            build_results = run_standard_ci_stage(project, branch,
+                                                  'build-artifacts')
+        }}
+        milestone()
+        def deploy_params = get_deploy_params(build_results)
+        stage('deploy-to-.*-snapshot') {{
+            run_standard_ci_stage(project, branch, 'deploy-to-.*-snapshot',
+                                  deploy_params)
+        }}
+    }}
 }}
 
 
-// This decorator allows using jenkins internal non-serializable objects
-// see https://github.com/jenkinsci/workflow-plugin/blob/master/TUTORIAL.md
 @NonCPS
-def get_jobs(project, version, trigger_stage) {{
-    def jobs = Jenkins.instance.items.findAll {{ job ->
-        job.name =~ /^${{project}}_${{version}}_${{trigger_stage}}/ \
+def get_jobs_names(project, version, trigger) {{
+    def jobs_names = Jenkins.instance.items.findAll {{ job ->
+        job.name =~ /^${{project}}_${{version}}_${{trigger}}/ \
         && job.name != env.JOB_NAME \
         && ! job.name.endsWith('-trigger')
     }}.collect {{ job ->
         job.name
     }}
+    return jobs_names
+}}
+
+def create_jobs(jobs_names, job_params) {{
+    def jobs = [:]
+    for (int i = 0; i < jobs_names.size(); i++) {{
+        // it is required to define 'job_name' in each iteration
+        // so it will be in the scope of the closure.
+        def job_name = jobs_names[i]
+        jobs[job_name] = {{ build( [job: job_name, parameters: job_params]) }}
+    }}
     return jobs
 }}
 
 
-def run_checks(project, branch, trigger_stage) {{
-    // runs the check jobs
-    stage concurrency: 5, name: 'Running checks'
-    def check_jobs = get_jobs(project, branch, trigger_stage)
-    println "Found ${{check_jobs.size()}} check jobs"
-    def checkers = [:]
-    for (int i = 0; i < check_jobs.size(); i++) {{
-        def job_name = check_jobs[i]
-        checkers[job_name] = {{
-            build([
-                job: job_name,
-                parameters: [
-                    [
-                        $class: 'StringParameterValue',
-                        name: 'GERRIT_REFSPEC',
-                        value: env.GERRIT_REFSPEC?: ''
-                    ],
-                    [
-                        $class: 'StringParameterValue',
-                        name: 'GERRIT_BRANCH',
-                        value: env.GERRIT_BRANCH?: ''
-                    ],
-                    [
-                        $class: 'StringParameterValue',
-                        name: 'REFSPEC',
-                        value: env.REFSPEC?: ''
-                    ],
-                    [
-                        $class: 'StringParameterValue',
-                        name: 'sha1',
-                        value: env.sha1?: ''
-                    ]
-                ]
-            ])
-        }}
-    }}
-    return parallel(checkers)
+def get_jobs(project, version, trigger_stage, job_params) {{
+   def names = get_jobs_names(project, version, trigger_stage)
+   return create_jobs(names, job_params)
 }}
-
-
-def build_artifacts(project, branch) {{
-    // runs the build jobs
-    stage concurrency: 5, name: 'Building'
-    def build_jobs = get_jobs(project, branch, 'build-artifacts')
-    println "Found ${{build_jobs.size()}} build jobs"
-    def builders = [:]
-    for (int i = 0; i < build_jobs.size(); i++) {{
-        def job_name = build_jobs[i]
-        builders[job_name] = {{
-            build([
-                job: job_name,
-                parameters: [
-                    [
-                        $class: 'StringParameterValue',
-                        name: 'GERRIT_REFSPEC',
-                        value: env.GERRIT_REFSPEC?: ''
-                    ],
-                    [
-                        $class: 'StringParameterValue',
-                        name: 'GERRIT_BRANCH',
-                        value: env.GERRIT_BRANCH?: ''
-                    ],
-                    [
-                        $class: 'StringParameterValue',
-                        name: 'REFSPEC',
-                        value: env.REFSPEC?: ''
-                    ],
-                    [
-                        $class: 'StringParameterValue',
-                        name: 'sha1',
-                        value: env.sha1?: ''
-                    ]
-
-                ]
-            ])
-        }}
-    }}
-    return parallel(builders)
-}}
-
 
 @NonCPS
-def extract_builders_list(build_results) {{
-    // this is not serializable, thus it has to go in a nonCPS method
-    // retrieve all the full urls of the builders, to pass to the deplayers
-    def builders_list = []
-    build_results.each {{ job ->
-        builders_list.add(
-            job.getValue().getAbsoluteUrl()
-        )
-    }}
-    return builders_list
+def get_deploy_params(build_results) {{
+    def build_urls = build_results.collect {{
+            it.getValue().getAbsoluteUrl()
+        }}.join(',')
+    return [ string(name: 'BUILD_ARTIFACTS_JOBS', value: build_urls),
+             string(name: 'BUILD_ID',
+                    value: "${{env.JOB_NAME}}-${{env.BUILD_NUMBER}}") ]
+}}
+
+def get_common_params() {{
+    return [ string(name: 'GERRIT_REFSPEC', value: params.GERRIT_REFSPEC?: ''),
+             string(name: 'GERRIT_BRANCH', value: params.GERRIT_BRANCH?: ''),
+             string(name: 'REFSPEC', value: params.REFSPEC),
+             string(name: 'sha1', value: params.sha1) ]
 }}
 
 
-def deploy(build_results, project, branch) {{
-    // Runs the deploy jobs with the build_results
-    builders_list = extract_builders_list(build_results)
-    stage concurrency: 1, name: 'Deploying to snapshot'
-    def deploy_jobs = get_jobs(project, branch, 'deploy-to-.*-snapshot')
-    println "Found ${{deploy_jobs.size()}} deploy jobs"
-    def deployers = [:]
-    for (int i = 0; i < deploy_jobs.size(); i++) {{
-        def job_name = deploy_jobs[i]
-        deployers[job_name] = {{
-            build([
-                job: job_name,
-                parameters: [
-                    [
-                        $class: 'StringParameterValue',
-                        name: 'BUILD_ARTIFACTS_JOBS',
-                        value: builders_list.join(',')
-                    ],
-                    [
-                        $class: 'StringParameterValue',
-                        name: 'BUILD_ID',
-                        value: "${{env.JOB_NAME}}-${{env.BUILD_NUMBER}}"
-                    ]
-                ]
-            ])
-        }}
+def run_standard_ci_stage(project, version, trigger, job_params=null) {{
+    if (job_params == null) {{
+        job_params = get_common_params()
     }}
-    return parallel(deployers)
+    def jobs = get_jobs(project, version, trigger, job_params)
+    if (jobs.size() < 1) {{
+        error ("Unable to find any jobs for project: $project," +
+                "version: $version, trigger: $trigger")
+    }}
+    return parallel(jobs)
 }}
 
-
-def main() {{
-    def check_results = run_checks(project, branch, trigger_stage)
-    if (with_build_and_deploy) {{
-        def build_results = build_artifacts(project, branch)
-        def deploy_results = deploy(build_results, project, branch)
-    }}
+@NonCPS
+def check_empty_params(vars) {{
+    return vars.findAll {{
+            params[it] == null || params[it].allWhitespace
+        }}.collect {{
+            "Missing parameter: $it"
+        }}.join('\n')
 }}
-
-// dummy useless main
-main()
