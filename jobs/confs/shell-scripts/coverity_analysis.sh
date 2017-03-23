6ed65451cebd05bc870f686764814dfd2333c2ff
Daniel Belenky <dbelenky@redhat.com>
2017-03-23 10:43:50 +0200

    jenkins: Split ovirt-engine-_coverity-analysis job
    As part of the effort to enable using the 'jenkins'
    repo to create different jobs for different Jenkins instances,
    I'm splitting some jobs that are defined inside the YAML dir
    instead of the 'standard' definition of 'project' and
    'job template'
    
    Jira: https://ovirt-jira.atlassian.net/browse/OVIRT-1264
    
    Change-Id: I666f261b6cbd250290341317528c467db21ff83a
    Signed-off-by: Daniel Belenky <dbelenky@redhat.com>

--- jobs/confs/shell-scripts/coverity_analysis.sh
+++ jobs/confs/shell-scripts/coverity_analysis.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -xe
 echo "shell-scripts/coverity_analysis.sh"
 
-mkdir -p "${WORKSPACE}/tmp"
-export TMPDIR="${WORKSPACE}/tmp"
+mkdir -p "${{WORKSPACE}}/tmp"
+export TMPDIR="${{WORKSPACE}}/tmp"
 
 MAVEN_OPTS="-XX:PermSize=512M "
 MAVEN_OPTS+="-XX:MaxPermSize=1024m "
 MAVEN_OPTS+="-Xms1024M -Xmx4096m "
-MAVEN_OPTS+="-Djava.io.tmpdir=$WORKSPACE/tmp "
+MAVEN_OPTS+="-Djava.io.tmpdir=${{WORKSPACE}}/tmp "
 MAVEN_OPTS+="-Dgwt.compiler.localWorkers=1 "
 MAVEN_OPTS+="-Dgwt.logLevel=TRACE"
 export MAVEN_OPTS
@@ -15,7 +15,7 @@ export MAVEN_OPTS
 wget -N https://scan.coverity.com/download/linux64 \
     --post-data "token=YqBYeyDp2jPuC_uVz0Hdog&project=ovirt-engine" \
     -O coverity_tool.tgz
-rm -rf "$WORKSPACE"/"cov-analysis-linux64-*"
+rm -rf "{{$WORKSPACE}}"/"cov-analysis-linux64-*"
 
 tar -xvf  coverity_tool.tgz
 
@@ -37,9 +37,9 @@ pushd ovirt-engine
     --dir cov-int \
     mvn clean install \
         -DskipTests=true \
-        -s ${WORKSPACE}/jenkins/xml/artifactory-ovirt-org-settings.xml \
-        -Dmaven.repo.local=${WORKSPACE}/.repository
-#--projects ${projects}
+        -s ${{WORKSPACE}}/jenkins/xml/artifactory-ovirt-org-settings.xml \
+        -Dmaven.repo.local=${{WORKSPACE}}/.repository
+#--projects ${{projects}}
 tar czvf ovirt-engine-cov.tgz cov-int
 curl \
     --form project=ovirt-engine \
