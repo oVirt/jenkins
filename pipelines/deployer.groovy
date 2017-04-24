b7e2dbade483b45abd89f4b8df1519474aba64c8
Barak Korren <bkorren@redhat.com>
2017-04-24 18:38:25 +0300

    Add more data to error emails from 'deploy_to' job
    Change-Id: Idf2c7c044ab6144079e72ad88f6273de740d4e61
    Signed-off-by: Barak Korren <bkorren@redhat.com>
    Jira-Ticket: https://ovirt-jira.atlassian.net/browse/OVIRT-1335

--- pipelines/deployer.groovy
+++ pipelines/deployer.groovy
@@ -247,8 +247,14 @@ def email_notify(status, recipients='infra@ovirt.org') {
             " - Build #${env.BUILD_NUMBER} - ${status}!",
         body: [
             "Build: ${env.BUILD_URL}",
-            "Build Number: ${env.BUILD_NUMBER}",
+            "Build Name: ${currentBuild.displayName}",
             "Build Status: ${status}",
+            "Gerrit change: ${params.GERRIT_CHANGE_URL}",
+            "- title: ${params.GERRIT_CHANGE_SUBJECT}",
+            "- project: ${params.GERRIT_PROJECT}",
+            "- branch: ${params.GERRIT_BRANCH}",
+            "- author: ${params.GERRIT_CHANGE_OWNER_NAME}" +
+            " <${params.GERRIT_CHANGE_OWNER_EMAIL}>",
         ].join("\n"),
         to: recipients,
         mimeType: 'text/plain'
