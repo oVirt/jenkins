28d0b911763057b7a04becf847853d57af4e3a25
Daniel Belenky <dbelenky@redhat.com>
2018-02-15 13:56:02 +0200

    Add a tool to check if a change is merged
    check_if_merged.sh is a tool that checks environmental variables which
    are set by Gerrit Trigger and returns 0 if the change pointed by
    $GERRIT_REFSPEC is merged and 1 otherwise.
    
    It will be used in standard-stage to skip whitelist checking for changes
    that already been merged.
    
    Jira-Ticket: https://ovirt-jira.atlassian.net/browse/OVIRT-1875
    Change-Id: Ib576e73cfc7197b6d42a7ea46e63fd1403391bae
    Signed-off-by: Daniel Belenky <dbelenky@redhat.com>

---
+++ scripts/check_if_merged.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -e
+echo "scritps/check_if_merged.sh"
+#
+# Check if a change pointed by GERRIT_REFSPEC is merged
+#
+GERRIT_PROJECT="${GERRIT_PROJECT:?Error GERRIT_PROJECT is unset.}"
+GERRIT_BRANCH="${GERRIT_BRANCH:?Error GERRIT_BRANCH is unset.}"
+GERRIT_REFSPEC="${GERRIT_REFSPEC:?Error GERRIT_REFSPEC is unset.}"
+GERRIT_NAME="${GERRIT_NAME:?Error GERRIT_NAME is unset.}"
+
+main() {
+    local workspace
+    if [[ ! "${WORKSPACE}" ]]; then
+        trap 'clean "$workspace"' EXIT HUP INT QUIT
+        workspace="$(mktemp -d --tmpdir stdci_check_is_merged.XXXXX)"
+    else
+        workspace="${WORKSPACE}"
+    fi
+
+    local path_to_project="${workspace}/${GERRIT_PROJECT##*/}"
+    git init "$path_to_project"
+    (
+        cd "$path_to_project"
+        local clone_url="https://${GERRIT_NAME}/${GERRIT_PROJECT}"
+        git fetch "$clone_url" "+refs/heads/${GERRIT_BRANCH}:__the-branch__"
+        git fetch "$clone_url" "+${GERRIT_REFSPEC}:__the-patch__"
+
+        local merge_base="$(git merge-base __the-branch__ __the-patch__)"
+        local patch_to_cmp="$(git rev-parse __the-patch__)"
+        if [[ "$merge_base" == "$patch_to_cmp" ]]; then
+            echo "$GERRIT_REFSPEC is merged"
+            return 0
+        fi
+        echo "$GERRIT_REFSPEC is not merged"
+        return 1
+    )
+}
+
+clean() {
+    local tmpdir="${1:?Error. Temp dir must be provided}"
+    rm -rf "$tmpdir"
+}
+
+main
