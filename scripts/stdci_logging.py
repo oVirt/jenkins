a06ad2c11d54704cb385b1909cfb5bff309aac88
Emil Natan <ena@redhat.com>
2018-05-08 16:58:49 +0300

    Collect STDCI logging functions into a separate module
    The same logging functions and classes existed in few modules.
    This patch collects the duplicate code in a separate module -
    scripts/scripts/stdci_logging.py
    
    Change-Id: I5216b1fe323540e19bf5cf546aeb045cc3f05f71
    Jira-ticket: https://ovirt-jira.atlassian.net/browse/OVIRT-1987
    Signed-off-by: Emil Natan <ena@redhat.com>

---
+++ scripts/stdci_logging.py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python
+"""stdci_logging.py - STDCI logging functions
+
+    Note: stdci_logging.py and usrc.py share the same functions definitions,
+    and that's on purpose. usrc.py should be self-contained.
+"""
+import argparse
+import logging
+import sys
+
+
+def add_logging_args(parser):
+    """Add logging-related command line argumenets
+
+    :param ArgumentParser parser: An argument parser to add the parameters to
+    """
+    parser.add_argument(
+        '-v', '--verbose', action='store_true', help='provide verbose output'
+    )
+    parser.add_argument(
+        '-d', '--debug', action='store_true', help='provide debugging output'
+    )
+    parser.add_argument(
+        '--log', nargs='?', const=sys.stderr, help=(
+            'Log to the specified file. If no filename is specified, output'
+            ' the regular output messages to STDERR in full log format.'
+        )
+    )
+
+
+def setup_console_logging(args, logger=None):
+    """Configure logging for when running as a console app
+
+    :param argparse.Namespace args: Argument parsing results for an
+                                    ArgumentParser object to which
+                                    add_logging_args had been applied.
+    :param logging.Logger logger:   (Optional) A logger to apply configuration
+                                    to. If unspecified, configuration will be
+                                    applied to the root logger.
+    """
+    if logger is None:
+        logger = logging.getLogger()
+    if args.debug:
+        level = logging.DEBUG
+    elif args.verbose:
+        level = logging.INFO
+    else:
+        level = logging.WARN
+    logger.setLevel(level)
+    stderr_handler = logging.StreamHandler(sys.stderr)
+    stderr_handler.setFormatter(ExceptionHider('%(message)s'))
+    logger.addHandler(stderr_handler)
+    if args.log is None:
+        pass
+    elif args.log == sys.stderr:
+        stderr_handler.setFormatter(ExceptionSpreader(
+            '%(asctime)s:%(levelname)s:%(name)s:%(message)s'
+        ))
+    else:
+        file_handler = logging.handlers.WatchedFileHandler(args.log)
+        file_handler.setFormatter(ExceptionSpreader(
+            '%(asctime)s:%(levelname)s:%(name)s:%(message)s'
+        ))
+        file_handler.setLevel(1)  # Set lowest possible level
+        stderr_handler.setLevel(level)
+        logger.setLevel(1)  # Set lowest possible level
+        logger.addHandler(file_handler)
+
+
+class BlockFormatter(logging.Formatter):
+    """A log formatter that knows how to handle text blocks that are embedded
+    in the log object
+    """
+    def format(self, record):
+        """Called by the logging.Handler object to do the actual log formatting
+
+        :param logging.LogRecord record: The log record to be formatted
+
+        This format generates extra log lines to display text blocks embedded
+        in the log object as indented text
+
+        :rtype: str
+        :returns: The string to be written to the log
+        """
+        blocks = getattr(record, 'blocks', None)
+        if blocks is None:
+            return super(BlockFormatter, self).format(record)
+        out_rec = copy(record)
+        exc_info = out_rec.exc_info
+        out_rec.exc_info = None
+        out = [super(BlockFormatter, self).format(out_rec)]
+        for block in self._iter_blocks(blocks):
+            if isinstance(block, string_types):
+                title, text = None, block
+            elif isinstance(block, Iterable) and len(block) == 2:
+                title, text = block
+            else:
+                title, text = None, block
+            if title is not None:
+                out.append(self._log_line(out_rec, '  ---- %s ----', title))
+            for line in text.splitlines():
+                out.append(self._log_line(out_rec, '    %s', line))
+        if exc_info:
+            out.append(self.formatException(exc_info))
+        return '\n'.join(out)
+
+    def _iter_blocks(self, blocks):
+        """Returns an iterator over any text blocks added to a log record
+
+        :param object blocks: An object representing text blocks, may be a
+                              single string, a Mapping an Iterable or any other
+                              object that can be converted into a string.
+        :rtype: Iterator
+        :returns: An iterator over blocks where each block my be either
+                  a string or a pair if a title string and a text string.
+        """
+        if isinstance(blocks, string_types):
+            return iter((blocks,))
+        elif isinstance(blocks, Mapping):
+            return iteritems(blocks)
+        elif isinstance(blocks, Iterable):
+            return blocks
+        else:
+            return iter((str(blocks),))
+
+    def _log_line(self, mut_rec, msg, line):
+        """format a single log line using the superclass formatter
+
+        :param logrecord mut_rec: a logrecord object that is allowed to be
+                                  mutated and contains the extra data about the
+                                  message we're logging
+        :param str msg:           a logger format string for the line to format
+        :param str line:          the log line to format
+        :type: str
+        :returns: a formatted log line
+        """
+        mut_rec.msg = msg
+        mut_rec.args = (line,)
+        return super(BlockFormatter, self).format(mut_rec)
+
+
+class ExceptionSpreader(BlockFormatter):
+    """A log formatter that takes care of properly formatting exception objects
+    if they are attached to logs
+    """
+    def format(self, record):
+        """Called by the logging.Handler object to do the actual log formatting
+
+        :param logging.LogRecord record: The log record to be formatted
+
+        This formatter converts the embedded excpetion opbject into a text
+        block and the uses the BlockFormatter to display it
+
+        :rtype: str
+        :returns: The string to be written to the log
+        """
+        if record.exc_info is None:
+            return super(ExceptionSpreader, self).format(record)
+        out_rec = copy(record)
+        out_rec.exc_info = None
+        if getattr(out_rec, 'blocks', None) is None:
+            out_rec.blocks = format_exception(*record.exc_info)
+        else:
+            out_rec.blocks = chain(
+                self._iter_blocks(out_rec.blocks),
+                (('excpetion', ''.join(format_exception(*record.exc_info))),)
+            )
+        return super(ExceptionSpreader, self).format(out_rec)
+
+
+class ExceptionHider(BlockFormatter):
+    """A log formatter that ensures that exception objects are not dumped into
+    the logs
+    """
+    def format(self, record):
+        """Called by the logging.Handler object to do the actual log formatting
+
+        :param logging.LogRecord record: The log record to be formatted
+
+        This formatter essentially strips away any embedded exception objects
+        from the record objects.
+
+        :rtype: str
+        :returns: The string to be written to the log
+        """
+        if record.exc_info is None:
+            return super(ExceptionHider, self).format(record)
+        out_rec = copy(record)
+        out_rec.exc_info = None
+        return super(ExceptionHider, self).format(out_rec)
