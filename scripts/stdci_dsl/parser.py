cbe75c5e6b812481295818fa7dadec8dc903c4db
Barak Korren <bkorren@redhat.com>
2018-07-18 12:57:34 +0300

    Move DSL normalization functions to common package
    The remove_case_and_signs and normalize_key_name functions can be useful
    for other parts of the STDCI DSL code, so moved them out of parser.py
    and into their own syntax_utils.py package.
    
    Change-Id: I5bce25d44261075ee3773b03396879fef657799e
    Signed-off-by: Barak Korren <bkorren@redhat.com>
    Jira-ticket: https://ovirt-jira.atlassian.net/browse/OVIRT-2343

--- scripts/stdci_dsl/parser.py
+++ scripts/stdci_dsl/parser.py
@@ -13,6 +13,7 @@ from yaml import safe_load
 from .options.parser_utils import get_merged_options
 from .job_thread import JobThread, STDCI_CATEGORIES
 from ..nested_config import gen_vectors
+from .syntax_utils import normalize_key_name, remove_case_and_signs
 
 
 logger = logging.getLogger(__name__)
@@ -97,7 +98,7 @@ def normalize_config_values(key, data):
         return data
     if isinstance(data, Mapping):
         return dict(
-            (_remove_case_and_signs(k), normalize_config_values(k, v))
+            (remove_case_and_signs(k), normalize_config_values(k, v))
             for (k, v) in iteritems(data)
         )
     elif isinstance(data, string_types):
@@ -120,54 +121,7 @@ def normalize_config_keys(key):
             'key must be string type! not {0}.\n'
             'Received: {1}'.format(type(key), key)
         )
-    key = _remove_case_and_signs(key)
-    return _normalize_key_name(key)
+    key = remove_case_and_signs(key)
+    return normalize_key_name(key)
 
 
-def _remove_case_and_signs(key):
-    """Given a string, transform it into lower case and remove whitespaces,
-    dashes and underscores.
-
-    :param str data_in: Option name or (string) value to remove case and signs
-                        from.
-
-    :rtype: str
-    :returns: String in lower case form and with signs removed
-    """
-    return key.lower().replace(' ', '').replace('-', '').replace('_', '')
-
-
-_KEY_NAME_TRANSLATIONS = {
-    'distro': 'distro',
-    'distros': 'distro',
-    'distribution': 'distro',
-    'distributions': 'distro',
-    'os': 'distro',
-    'operatingsystem': 'distro',
-    'operatingsystems': 'distro',
-    'arch': 'arch',
-    'archs': 'arch',
-    'architecture': 'arch',
-    'architectures': 'arch',
-    'stage': 'stage',
-    'stages': 'stage',
-    'substage': 'substage',
-    'substages': 'substage',
-}
-
-
-def _normalize_key_name(key, translations_map=None):
-    """Given key and a translations map (optional) will extract this key
-    from the given map, or return the key itself if it's not in the map.
-
-    :param str key: Key to normalize
-    :param Mapping translations_map: Map from abstract forms of different keys
-                                     to the normal value of the key.
-
-    :rtype: str
-    :returns: The given key in it's normalized form if it exists.
-              Otherwise, return the key as is.
-    """
-    if not translations_map:
-        translations_map = _KEY_NAME_TRANSLATIONS
-    return translations_map.get(key, key)
---
+++ scripts/stdci_dsl/syntax_utils.py
@@ -0,0 +1,51 @@
+#!/bin/env python
+"""syntax_utils.py - Utilities for parsing the STDCI DSL syntax"""
+
+
+def remove_case_and_signs(key):
+    """Given a string, transform it into lower case and remove whitespaces,
+    dashes and underscores.
+
+    :param str data_in: Option name or (string) value to remove case and signs
+                        from.
+
+    :rtype: str
+    :returns: String in lower case form and with signs removed
+    """
+    return key.lower().replace(' ', '').replace('-', '').replace('_', '')
+
+
+_KEY_NAME_TRANSLATIONS = {
+    'distro': 'distro',
+    'distros': 'distro',
+    'distribution': 'distro',
+    'distributions': 'distro',
+    'os': 'distro',
+    'operatingsystem': 'distro',
+    'operatingsystems': 'distro',
+    'arch': 'arch',
+    'archs': 'arch',
+    'architecture': 'arch',
+    'architectures': 'arch',
+    'stage': 'stage',
+    'stages': 'stage',
+    'substage': 'substage',
+    'substages': 'substage',
+}
+
+
+def normalize_key_name(key, translations_map=None):
+    """Given key and a translations map (optional) will extract this key
+    from the given map, or return the key itself if it's not in the map.
+
+    :param str key: Key to normalize
+    :param Mapping translations_map: Map from abstract forms of different keys
+                                     to the normal value of the key.
+
+    :rtype: str
+    :returns: The given key in it's normalized form if it exists.
+              Otherwise, return the key as is.
+    """
+    if not translations_map:
+        translations_map = _KEY_NAME_TRANSLATIONS
+    return translations_map.get(key, key)
