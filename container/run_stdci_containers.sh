6203fd96d2de4445aaeacfa08dc7d8e325fef7f1
Daniel Belenky <dbelenky@redhat.com>
2018-07-31 12:11:53 +0300

    Single slot deployment in run_stdci_containers
    Change-Id: I59e03c9c1ae50e7778755e347cd691344f0d7c23
    Jira-Ticket: https://ovirt-jira.atlassian.net/browse/OVIRT-2384
    Signed-off-by: Daniel Belenky <dbelenky@redhat.com>

--- container/run_stdci_containers.sh
+++ container/run_stdci_containers.sh
@@ -11,33 +11,74 @@ STDCI_IMAGE_NAME="stdci:current-slave-image"
 RUNTIME_USERNAME=jenkins
 
 main() {
+    local command="${1:?}"
+    local command_args=("${@:2}")
+
+    mkdir -p "$STDCI_DATA_DIR"
+
+    cmd_$command "${command_args[@]}"
+}
+
+cmd_deploy_all() {
     local jenkins_server="${1:?ERROR: You have to specify jenkins server url}"
     shift
     local secrets=("${@}");
     local arg_count=${#@}
-    mkdir -p "$STDCI_DATA_DIR"
 
     local hostname; hostname="$(hostname)"
     local base_cmd container_name extras
     for secret in "${secrets[@]}"; do
-
-        container_name="$hostname-container-${arg_count}"
-        local extras; IFS=" " read -r -a extras <<< \
-            "$(prep_slave_slot "$arg_count" "$secret")"
-
-        base_cmd=(
-            --privileged "--name=$container_name" -d
-            -e "STDCI_SLAVE_CONTAINER_NAME=$container_name"
-            -e "JENKINS_URL=$jenkins_server"
-            -e "JENKINS_AGENT_NAME=$container_name"
-            -e "JENKINS_SECRET=$secret"
-            -e "CI_RUNTIME_UNAME=$RUNTIME_USERNAME"
-        )
-
-        docker run "${base_cmd[@]}" "${extras[@]}" "$STDCI_IMAGE_NAME"
-
+        cmd_deploy_slot "$arg_count" "$jenkins_server" "$secret"
         (( --arg_count ))
     done
+}
+
+cmd_deploy_slot() {
+    local slot_id="${1:?ERROR. You must provide slot ID}"
+    local jenkins_server="${2:?ERROR: You have to specify jenkins server url}"
+    local secret="${3:?ERROR. You must provide a secret}"
+
+    cmd_verify_slot_free "$slot_id" || return 1
+
+    local extras; IFS=" " read -r -a extras <<< \
+        "$(prep_slave_slot "$slot_id" "$secret")"
+
+    local container_name="$(get_container_name "$slot_id")"
+    base_cmd=(
+        --privileged "--name=$container_name" -d
+        -e "STDCI_SLAVE_CONTAINER_NAME=$container_name"
+        -e "JENKINS_URL=$jenkins_server"
+        -e "JENKINS_AGENT_NAME=$container_name"
+        -e "JENKINS_SECRET=$secret"
+        -e "CI_RUNTIME_UNAME=$RUNTIME_USERNAME"
+    )
+
+    docker run "${base_cmd[@]}" "${extras[@]}" "$STDCI_IMAGE_NAME"
+}
+
+cmd_verify_slot_free() {
+    local slot_id="${1:?ERROR. You must provide slot ID}"
+    local container_name="$(get_container_name "$slot_id")"
+    local container_id="$(
+        docker ps --filter="name=$container_name" --format="{{.ID}}"
+    )"
+    if [[ -n "$container_id" ]]; then
+        local answer
+        echo "There is a running container on slot $slot_id."
+        printf "Do you want to remove it? [y/N]? "
+        read -r answer
+        if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
+            echo "Trying to remove: $container_name ($container_id)"
+            docker rm "$container_id" || {
+                echo "Failed to remove the container"
+                return 1
+            }
+        else
+            echo "Slot is already in use by $container_name ($container_id)"
+            return 1
+        fi
+    fi
+    echo "Slot $slot_id is free"
 }
 
 prep_slave_slot() {
@@ -115,6 +156,13 @@ prep_docker_dirs() {
         -v "$real_docker_home:/var/lib/docker"
     )
     printf "%s " "${cmd[@]}"
+}
+
+get_container_name() {
+    local slot_id="${1:?ERROR. You must provide splot ID}"
+    local hostname="$(hostname)"
+    local container_name="${hostname}-container-${slot_id}"
+    echo "$container_name"
 }
 
 main "$@"
