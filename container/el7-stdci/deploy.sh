6505dabb44d215d680de631346cd72034c2de846
Daniel Belenky <dbelenky@redhat.com>
2018-08-01 16:18:47 +0300

    stdci-container: Setup firewalld
    Change-Id: Ic88829c6b09a91a0d7577cd8686b50939c94df1e
    Signed-off-by: Daniel Belenky <dbelenky@redhat.com>

--- container/el7-stdci/deploy.sh
+++ container/el7-stdci/deploy.sh
@@ -1,10 +1,22 @@
 #!/bin/bash -ex
 
 main(){
+    enable_firewalld_service
     enable_docker_service
     enable_environment_service
     enable_jenkins_home_service
     enable_jnlp_slave
+}
+
+enable_firewalld_service() {
+    local service_name
+    for firewalld_service in /etc/firewalld/services/*; do
+        service_name="${firewalld_service%%.*}"
+        service_name="${service_name##*/}"
+        firewall-offline-cmd --zone=public -q \
+            --add-service="${service_name}"
+    done
+    systemctl enable firewalld.service
 }
 
 enable_docker_sock_service() {
