a1827d6f4d6b99a44ef154490219aca2177247bb
Daniel Belenky <dbelenky@redhat.com>
2018-12-26 14:32:43 +0200

    Add el7-loader-node container
    - Added Dockerfile to build loader node
    - Added basic CI and deployment
    
    Change-Id: I8cb6c77a2fe8cdeb3828384012cb84ab0738c50d
    Signed-off-by: Daniel Belenky <dbelenky@redhat.com>

---
+++ automation/verify-build-containers.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -xe
+
+main() {
+    local substage_name stage_name
+    stage_name="$(basename "$0")"
+    stage_name="${stage_name%%.*}"
+    substage_name="$(basename "$0" | cut -d . -f 2)"
+
+    local image_tag=testing
+    if [[ "$stage_name" = build-artifacts ]]; then
+        image_tag=exported-artifacts
+    fi
+
+    build_image "$substage_name" "$image_tag"
+    "verify_${substage_name//-/_}" "$substage_name" "$image_tag"
+}
+
+build_image() {
+    local image_to_build="${1:?}"
+    local tag="${2:-testing}"
+
+    (
+        cd container/"${image_to_build}"
+        docker build -t "${image_to_build}":"${tag}" .
+    )
+}
+
+verify_el7_loader_node() {
+    local image="${1:?}"
+    local tag="${2:?}"
+    local jenkins_repo="https://gerrit.ovirt.org/jenkins"
+
+    docker run -i --rm "${image}:${tag}" bash -c "
+        git clone $jenkins_repo
+        cd jenkins
+        ./scripts/usrc.py get
+    "
+}
+
+main "$@"
