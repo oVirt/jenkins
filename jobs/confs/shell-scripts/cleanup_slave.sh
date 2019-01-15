0377f0fa7dee75f2814ddbfc60ae4db51e650bde
Barak Korren <bkorren@redhat.com>
2019-01-15 11:22:43 +0200

    slave_cleanup: Add mock cleanup functions
    We had some general cleanup functions in `mock_cleanup.sh` that clean up
    chroots and caches other then the ones used in the current job.
    
    Since `mock_cleanup.sh` runs rather late, there are situations where
    those things could be left on the slave. Adding the functions to
    `slave_cleanup.sh` makes sure they are removed before the next job
    starts.
    
    Change-Id: If6876be46843750e2574a90a5c8e6e2878052406
    Signed-off-by: Barak Korren <bkorren@redhat.com>
    Jira-ticket: https://ovirt-jira.atlassian.net/browse/OVIRT-2649

--- jobs/confs/shell-scripts/cleanup_slave.sh
+++ jobs/confs/shell-scripts/cleanup_slave.sh
@@ -350,6 +350,78 @@ cleanup_dev_shm() {
     done
 }
 
+UMOUNT_RETRIES="${UMOUNT_RETRIES:-3}"
+UMOUNT_RETRY_DELAY="${UMOUNT_RETRY_DELAY:-1s}"
+
+safe_umount() {
+    local mount="${1:?}"
+    local attempt
+    for ((attempt=0 ; attempt < $UMOUNT_RETRIES ; attempt++)); do
+        # If this is not the 1st time through the loop, Sleep a while to let
+        # the problem "solve itself"
+        [[ attempt > 0 ]] && sleep "$UMOUNT_RETRY_DELAY"
+        # Try to umount
+        sudo -n umount --lazy "$mount" && return 0
+        # See if the mount is already not there despite failing
+        findmnt --kernel --first-only "$mount" > /dev/null || return 0
+    done
+    echo "ERROR:  Failed to umount $mount."
+    return 1
+}
+
+wipe_other_chroots() {
+    # Clean any leftover chroot from other jobs
+    if ! can_sudo rm; then
+        echo "Skipping cleanup of mock chroots - no sudo permissions"
+        return
+    fi
+    local mock_root this_chroot_failed mount mounts
+    local failed=false
+
+    for mock_root in /var/lib/mock/*; do
+        this_chroot_failed=false
+        mounts=($(cut -d\  -f2 /proc/mounts | grep "$mock_root" | sort -r)) || :
+        if [[ "$mounts" ]]; then
+            echo "Found mounted dirs inside the chroot $mock_root." \
+                 "Trying to umount."
+        fi
+        for mount in "${mounts[@]}"; do
+            safe_umount "$mount" && continue
+            # If we got here, we failed $UMOUNT_RETRIES attempts so we should make
+            # noise
+            failed=true
+            this_chroot_failed=true
+        done
+        if ! $this_chroot_failed; then
+            sudo -n rm -rf "$mock_root"
+        fi
+    done
+    # Return $failed from this function
+    ! $failed
+}
+
+wipe_old_caches() {
+    # remove mock caches that are older then 2 days:
+    if ! can_sudo rm; then
+        echo "Skipping cleanup of mock caches - no sudo permissions"
+        return
+    fi
+    find /var/cache/mock/ -mindepth 1 -maxdepth 1 -type d -mtime +2 -print0 | \
+        xargs -0 -tr sudo -n rm -rf
+    # We make no effort to leave around caches that may still be in use because
+    # packages installed in them may go out of date, so may as well recreate them
+}
+
+cleanup_mock() {
+    local failed=false
+
+    wipe_other_chroots || failed=true
+    wipe_old_caches || failed=true
+
+    # Return $failed from this function
+    ! $failed
+}
+
 can_sudo() {
     local cmd
 
@@ -394,6 +466,7 @@ main() {
     kill_lago_processes || failed=true
     cleanup_old_artifacts || failed=true
     cleanup_dev_shm || failed=true
+    cleanup_mock || failed=true
     echo "---------------------------------------------------------------"
     sudo -n df -h || df -h || :
     if $failed; then
