79ffdc7124302783d34d6744733171ecf15ade0c
Barak Korren <bkorren@redhat.com>
2019-02-11 23:42:36 +0200

    git_commit_files: Added role
    Added a generic Ansible role that contains a module for committing files
    into Git. The module is useful for generating the kind of commits that
    `poll-upstream-sources` jobs are expected to generate.
    
    Change-Id: I0cddbe252402385a3a028e8dfd9ea7f870ab3179
    Signed-off-by: Barak Korren <bkorren@redhat.com>

---
+++ playbooks/roles/git_commit_files/library/git_commit_files.py
@@ -0,0 +1,282 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# (c) 2018, Barak Korren <bkorren@redhat.com>
+# GNU General Public License v3.0+
+# (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
+DOCUMENTATION = '''
+module: git_commit_files
+
+short_description: Commit files int Git
+
+version_added: "2.8"
+
+author:
+    - "Barak Korren <bkorren@redhat.com>"
+
+description:
+    - Commit a given set of files into Git
+    - Can create a branch to commit into
+
+options:
+    files:
+        description:
+            - The list of files to commit
+            - An alternative to specifying files directly in free form
+            - This cannot be given of the free form is used and vice-versa
+        required: yes
+    repo_dir:
+        description:
+            - The directory where the Git repo resides
+            - If unspecified, the directory where ansible is running the module
+              from would be used
+        required: no
+    branch:
+        description:
+            - A branch that will be created and the commit added into
+            - If the branch exists it will be destroyed
+            - If unspecified the change will be committed to current branch
+        required: no
+        default: unspecified
+    commit_message:
+        description:
+            - The commit message to set when committing changes
+            - If not given a default message would be generated
+        required: no
+    add_change_id:
+        description:
+            - Set wither to add a 'Change-Id' header to the commit message
+            - The change id value will consist of a checksum of the modified
+              files
+        required: no
+        default: false
+    change_id_headers:
+        description:
+            - The list of headers to place change checksum into
+            - Setting 'add_change_id' effectively sets this to ['Change-Id']
+        required: no
+        default: []
+    add_headers:
+        description:
+            - A dictionary of headers to add to the commit message
+        required: no
+        default: {}
+
+requirements:
+  - I(git) installed on the target host
+  - The target work directory should already be a Git repo
+'''
+
+EXAMPLES = '''
+- name: Commit a set of files
+  git_commit_files:
+    files:
+      - file1.txt
+      - file2.txt
+
+- name: Commit with a custom message
+  git_commit_files
+    files: files1.txt
+    commit_message: "file1.txt was updated"
+
+- name: Commit with Change-Id and custom headers
+  git_commit_files:
+    files:
+      - file1.txt
+      - file2.txt
+    add_change_id: true
+    add_headers:
+      Automerge: yes
+'''
+
+RETURN = '''
+changed_files:
+    description:
+        - The list of files that were actually changed and committed
+    returned: success
+    type: dict
+'''
+import os
+from six.moves import filter
+from six import iteritems
+from hashlib import sha256
+
+from ansible.module_utils.basic import AnsibleModule, heuristic_log_sanitize
+
+
+class GitProcessError(RuntimeError):
+    def __init__(self, cmd, rc, stdout, stderr):
+        self.cmd = cmd
+        self.rc = rc
+        self.stdout = stdout
+        self.stderr = stderr
+
+    def __str__(self):
+        return 'Git command {} failed with {} exit code'.format(
+            self.cmd, self.rc
+        )
+
+    def fail_json(self, module):
+        msg = heuristic_log_sanitize(
+            self.stderr.rstrip(), module.no_log_values
+        )
+        module.fail_json(
+            git_cmd=module._clean_args(self.cmd),
+            rc=self.rc,
+            stdout=self.stdout,
+            stderr=self.stderr,
+            msg=msg
+        )
+
+
+class GitCommitFilesModule(AnsibleModule):
+    def __init__(self, *args, **kwargs):
+        kwargs['argument_spec'] = self.argspec
+        kwargs['supports_check_mode'] = False
+        AnsibleModule.__init__(self, *args, **kwargs)
+
+    def execute_module(self):
+        self._execute_module_w_args(**self.params)
+
+    def _execute_module_w_args(
+        self, files, repo_dir, branch, commit_message,
+        add_change_id, change_id_headers, add_headers,
+    ):
+        if not files:
+            self.fail_json(rc=256, msg='no files given')
+
+        if repo_dir and repo_dir.strip():
+            os.chdir(repo_dir.strip())
+
+        change_id_headers = set(change_id_headers)
+        if add_change_id:
+            change_id_headers.add('Change-Id')
+
+        try:
+            commit_data = self._commit_files(
+                files, branch, commit_message, change_id_headers, add_headers
+            )
+            self.exit_json(
+                changed=bool(commit_data['changed_files']),
+                **commit_data
+            )
+        except IOError as e:
+            if e.errno == 2:
+                self.fail_json(
+                    msg=str(e),
+                    file=e.filename,
+                    cwd=os.getcwd(),
+                )
+            else:
+                raise
+        except GitProcessError as e:
+            e.fail_json(self)
+
+    @property
+    def argspec(self):
+        args = dict(
+            files=dict(type='list', required=True),
+            repo_dir=dict(type='path'),
+            branch=dict(type='str'),
+            commit_message=dict(type="str"),
+            add_change_id=dict(type="bool", default=False),
+            change_id_headers=dict(type="list", default=[]),
+            add_headers=dict(type="dict", default={}),
+        )
+        return args
+
+    def _commit_files(
+        self, files, branch=None, commit_message=None,
+        change_id_headers=None, extra_headers=None,
+    ):
+        # Clear git index before adding the requested files
+        self._git('reset', 'HEAD')
+        self._git('add', *filter(os.path.exists, files))
+        changed_files = self._staged_files()
+        if changed_files:
+            if branch:
+                self._git('checkout', '-B', branch)
+            self._git('commit', '-m', self._commit_message(
+                changed_files, commit_message, change_id_headers, extra_headers
+            ))
+        return {'changed_files': changed_files}
+
+    def _staged_files(self):
+        return self._git('diff', '--staged', '--name-only').splitlines()
+
+    def _commit_message(
+        self, changed_files, commit_message,
+        change_id_headers=None, extra_headers=None
+    ):
+        if commit_message:
+            commit_message = str(commit_message).strip()
+        if not commit_message:
+            commit_message = self._commit_title(changed_files)
+            if len(changed_files) > 1:
+                commit_message += '\n\nChanged files:\n'
+                commit_message += '\n'.join(
+                    '- ' + fil for fil in changed_files
+                )
+        headers = self._commit_headers(
+            changed_files, change_id_headers, extra_headers
+        )
+        if headers:
+            commit_message += '\n'
+            commit_message += headers
+        return commit_message
+
+    def _commit_title(self, changed_files, max_title=60):
+        if len(changed_files) != 1:
+            return 'Changed {} files'.format(len(changed_files))
+        title = 'Changed: {}'.format(changed_files[0])
+        if len(title) <= max_title:
+            return title
+        title = 'Changed: {}'.format(os.path.basename(changed_files[0]))
+        if len(title) <= max_title:
+            return title
+        return 'Changed one file'
+
+    def _commit_headers(self, changed_files, change_id_headers, extra_headers):
+        headers = ''
+        if changed_files and change_id_headers:
+            checksum = self._files_checksum(changed_files)
+            for hdr in sorted(set(change_id_headers)):
+                headers += '\n{}: {}'.format(hdr, checksum)
+        if extra_headers:
+            for hdr, val in sorted(iteritems(extra_headers)):
+                headers += '\n{}: {}'.format(hdr, val)
+        return headers
+
+    def _files_checksum(self, changed_files):
+        digest = sha256()
+        for fil in sorted(set(changed_files)):
+            digest.update(fil.encode('utf-8'))
+            with open(fil, 'rb') as f:
+                digest.update(f.read())
+        return digest.hexdigest()
+
+    def _git(self, *args, **kwargs):
+        rc_args = ['git']
+        rc_args.extend(args)
+        rc, stdout, stderr = self.run_command(rc_args, **kwargs)
+        if rc != 0:
+            raise GitProcessError(rc_args, rc, stdout, stderr)
+        return stdout
+
+
+def main():
+    GitCommitFilesModule().execute_module()
+
+
+if __name__ == '__main__':
+    main()
---
+++ playbooks/roles/git_commit_files/test/library/test_git_commit_files.py
@@ -0,0 +1,328 @@
+"""test_git_commit_files.py - Tests for the git_commit_files Ansible module
+"""
+from __future__ import absolute_import, division, print_function
+import pytest
+from collections import Mapping
+from six import string_types
+
+import ansible.module_utils.basic
+from git_commit_files import GitCommitFilesModule
+
+
+class TestGitCommitFileModule(object):
+    @pytest.fixture
+    def repo(self, gitrepo, monkeypatch):
+        r = gitrepo(
+            'repo',
+            {
+                'msg': 'First commit',
+                'files': {
+                    'file1': 'File 1 content\n',
+                    'file2': 'File 2 content\n',
+                    'dir1/file11': 'File 11 content\n',
+                    'dir1/file12': 'File 12 content\n',
+                },
+            },
+        )
+        monkeypatch.chdir(r)
+        return r
+
+    @pytest.fixture
+    def instance(self, monkeypatch):
+        def _load_params():
+            return {'files': []}
+
+        monkeypatch.setattr(
+            ansible.module_utils.basic, '_load_params', _load_params
+        )
+        return GitCommitFilesModule()
+
+    @pytest.mark.parametrize('files,change,touch,changed,commits', [
+        ('', 'file1', '', '', 1),
+        ('file1', 'file1', '', 'file1', 2),
+        ('file1', '', 'file1', '', 1),
+        ('file1 file2', 'file1', '', 'file1', 2),
+        ('file1 file2', 'file1', 'file2', 'file1', 2),
+        ('file1 file2', 'file1 file2', '', 'file1 file2', 2),
+        ('file1', 'file1 file2', '', 'file1', 2),
+        ('file1 file3', 'file1 file3', '', 'file1 file3', 2),
+        ('file1 file2', 'file1 file3', '', 'file1', 2),
+        ('file3', 'file3', '', 'file3', 2),
+        ('file1 file3', 'file3', '', 'file3', 2),
+        ('dir1/file11 file2', 'dir1/file11', '', 'dir1/file11', 2),
+        ('dir1', 'dir1/file11 dir1/file12', '', 'dir1/file11 dir1/file12', 2),
+        ('dir1', 'dir1/file11', 'dir1/file12', 'dir1/file11', 2),
+        ('file1', 'dir1/file11 dir1/file12', '', '', 1),
+        ('file1 no_exist', 'file1', '', 'file1', 2),
+        ('file1 no_exist', '', '', '', 1),
+    ])
+    def test__commit_files(
+        self, git, repo, instance, files, change, touch, changed, commits
+    ):
+        files = files.split()
+        change = change.split()
+        touch = touch.split()
+        changed = changed.split()
+
+        for f in change:
+            (repo / f).write('Changed {} content'.format(f), ensure=True)
+        for f in touch:
+            fp = (repo / f)
+            fp.write(fp.read())
+
+        out = instance._commit_files(files)
+        assert isinstance(out, Mapping)
+        assert out['changed_files'] == changed
+        assert len(git('log', '--oneline').splitlines()) == commits
+        if changed:
+            assert sorted((
+                git('log', '-1', '--pretty=format:', '--name-only')
+            ).strip().splitlines()) == sorted(changed)
+            last_messgae = git('log', '-1', '--pretty=format:%s')
+            if len(changed) == 1:
+                assert last_messgae == 'Changed: {}'.format(changed[0])
+            else:
+                assert last_messgae == 'Changed {} files'.format(len(changed))
+
+    @pytest.mark.parametrize('files,add,change,changed,commits', [
+        ('file1', 'file2', 'file1', 'file1', 2),
+        ('file1', 'file1', 'file1', 'file1', 2),
+        ('file1', 'file2', '', '', 1),
+        ('file1', 'file3', 'file1', 'file1', 2),
+    ])
+    def test__commit_files_pre_added(
+        self, git, repo, instance, files, add, change, changed, commits
+    ):
+        files = files.split()
+        add = add.split()
+        change = change.split()
+        changed = changed.split()
+
+        for f in add:
+            (repo / f).write('Added {} content'.format(f), ensure=True)
+            git('add', f)
+        for f in change:
+            (repo / f).write('Changed {} content'.format(f), ensure=True)
+
+        out = instance._commit_files(files)
+        assert isinstance(out, Mapping)
+        assert out['changed_files'] == changed
+        assert len(git('log', '--oneline').splitlines()) == commits
+        if changed:
+            assert sorted((
+                git('log', '-1', '--pretty=format:', '--name-only')
+            ).strip().splitlines()) == sorted(changed)
+            last_messgae = git('log', '-1', '--pretty=format:%s')
+            if len(changed) == 1:
+                assert last_messgae == 'Changed: {}'.format(changed[0])
+            else:
+                assert last_messgae == 'Changed {} files'.format(len(changed))
+
+    @pytest.mark.parametrize('changed_files,expected', [
+        ('a_file.txt', 'Changed: a_file.txt'),
+        ('fil1 fil2 fil3', 'Changed 3 files'),
+        ('a_dir/a_file.txt', 'Changed: a_dir/a_file.txt'),
+        (
+            'a_really/rediculesly/insanely/extremely/very/long/file_path.txt',
+            'Changed: file_path.txt'
+        ),
+        (
+            'a_really_rediculesly_insanely_extremely_very_long_file_name.txt',
+            'Changed one file'
+        ),
+    ])
+    def test__commit_title(self, instance, changed_files, expected):
+        out = instance._commit_title(changed_files.split())
+        assert out == expected
+
+    def test__commit_files_branch(self, repo, instance, git, git_last_sha):
+        instance._commit_files(['file1'], 'tmp_branch')
+        assert len(git('log', '--oneline').splitlines()) == 1
+        assert git('symbolic-ref', 'HEAD').strip() == 'refs/heads/master'
+
+        (repo / 'file1').write('Changed content')
+        instance._commit_files(['file1'], 'tmp_branch')
+        assert len(git('log', '--oneline').splitlines()) == 2
+        assert git('symbolic-ref', 'HEAD').strip() == 'refs/heads/tmp_branch'
+        tmp_branch_sha = git_last_sha(repo)
+
+        git('checkout', 'master')
+        assert len(git('log', '--oneline').splitlines()) == 1
+        (repo / 'file1').write('Changed content again')
+        instance._commit_files(['file1'], 'tmp_branch')
+        assert len(git('log', '--oneline').splitlines()) == 2
+        assert git('symbolic-ref', 'HEAD').strip() == 'refs/heads/tmp_branch'
+        assert git_last_sha(repo) != tmp_branch_sha
+
+        (repo / 'file1').write('Changed content once more')
+        instance._commit_files(['file1'], 'tmp_branch')
+        assert len(git('log', '--oneline').splitlines()) == 3
+        assert git('symbolic-ref', 'HEAD').strip() == 'refs/heads/tmp_branch'
+
+        git('checkout', 'master')
+        assert len(git('log', '--oneline').splitlines()) == 1
+        (repo / 'file1').write('Changed content')
+        instance._commit_files(['file1'])
+        assert len(git('log', '--oneline').splitlines()) == 2
+        assert git('symbolic-ref', 'HEAD').strip() == 'refs/heads/master'
+
+    @pytest.mark.parametrize('changed_files,commit_message,expected', [
+        (
+            'file1', 'GIVEN_COMMIT_MESSGAE',
+            'GIVEN_COMMIT_MESSGAE\nHEADERS_GO_HERE'
+        ),
+        (
+            'file1', 'GIVEN_COMMIT_MESSGAE\n',
+            'GIVEN_COMMIT_MESSGAE\nHEADERS_GO_HERE'
+        ),
+        (
+            'file1', 'GIVEN_COMMIT_MESSGAE\n\nBODY',
+            'GIVEN_COMMIT_MESSGAE\n\nBODY\nHEADERS_GO_HERE'
+        ),
+        (
+            'file1', 'GIVEN_COMMIT_MESSGAE\n\nBODY\n  \n\n',
+            'GIVEN_COMMIT_MESSGAE\n\nBODY\nHEADERS_GO_HERE'
+        ),
+        (
+            'file1', None,
+            'TITLE_GOES_HERE\nHEADERS_GO_HERE'
+        ),
+        (
+            'file1 file2', None,
+            'TITLE_GOES_HERE\n\n'
+            'Changed files:\n- file1\n- file2\n'
+            'HEADERS_GO_HERE'
+        ),
+    ])
+    def test__commit_message(
+        self, monkeypatch, instance, changed_files, commit_message, expected
+    ):
+        monkeypatch.setattr(
+            instance, '_commit_title', lambda *a: 'TITLE_GOES_HERE'
+        )
+        monkeypatch.setattr(
+            instance, '_commit_headers', lambda *a: 'HEADERS_GO_HERE'
+        )
+        out = instance._commit_message(changed_files.split(), commit_message)
+        assert out == expected
+
+    @pytest.mark.parametrize('changed_files,commit_message,expected', [
+        (
+            'file1', 'GIVEN_COMMIT_MESSGAE',
+            'GIVEN_COMMIT_MESSGAE'
+        ),
+        (
+            'file1', 'GIVEN_COMMIT_MESSGAE\n',
+            'GIVEN_COMMIT_MESSGAE'
+        ),
+        (
+            'file1', 'GIVEN_COMMIT_MESSGAE\n\nBODY',
+            'GIVEN_COMMIT_MESSGAE\n\nBODY'
+        ),
+        (
+            'file1', 'GIVEN_COMMIT_MESSGAE\n\nBODY\n  \n\n',
+            'GIVEN_COMMIT_MESSGAE\n\nBODY'
+        ),
+        (
+            'file1', None,
+            'TITLE_GOES_HERE'
+        ),
+        (
+            'file1 file2', None,
+            'TITLE_GOES_HERE\n\n'
+            'Changed files:\n- file1\n- file2'
+        ),
+    ])
+    def test__commit_message_no_headers(
+        self, monkeypatch, instance, changed_files, commit_message, expected
+    ):
+        monkeypatch.setattr(
+            instance, '_commit_title', lambda *a: 'TITLE_GOES_HERE'
+        )
+        monkeypatch.setattr(instance, '_commit_headers', lambda *a: '')
+        out = instance._commit_message(changed_files.split(), commit_message)
+        assert out == expected
+
+    @pytest.mark.parametrize(
+        'changed_files,change_id_headers,extra_headers,expected',
+        [
+            ('file1', '', {}, ''),
+            (
+                'file1', 'Change-Id', {},
+                '\nChange-Id: CHECKSUM_GOES_HERE'
+            ),
+            (
+                'file1', 'Change-Id x-md5', {},
+                '\nChange-Id: CHECKSUM_GOES_HERE'
+                '\nx-md5: CHECKSUM_GOES_HERE'
+            ),
+            (
+                'file1', 'x-md5 Change-Id', {},
+                '\nChange-Id: CHECKSUM_GOES_HERE'
+                '\nx-md5: CHECKSUM_GOES_HERE'
+            ),
+            (
+                'file1', 'x-md5 Change-Id x-md5', {},
+                '\nChange-Id: CHECKSUM_GOES_HERE'
+                '\nx-md5: CHECKSUM_GOES_HERE'
+            ),
+            (
+                'file1', 'Change-Id', {'B_hdr': 'val1', 'A_hdr': 7},
+                '\nChange-Id: CHECKSUM_GOES_HERE'
+                '\nA_hdr: 7'
+                '\nB_hdr: val1'
+            ),
+            ('', '', {}, ''),
+            ('', 'Change-Id', {}, ''),
+            (
+                '', 'Change-Id', {'B_hdr': 'val1', 'A_hdr': 7},
+                '\nA_hdr: 7'
+                '\nB_hdr: val1'
+            ),
+        ]
+    )
+    def test__commit_headers(
+        self, monkeypatch, instance, changed_files, change_id_headers,
+        extra_headers, expected
+    ):
+        monkeypatch.setattr(
+            instance, '_files_checksum', lambda *a: 'CHECKSUM_GOES_HERE'
+        )
+        out = instance._commit_headers(
+            changed_files.split(), change_id_headers.split(), extra_headers
+        )
+        assert out == expected
+
+    def test__files_checksum(self, repo, instance):
+        checksum_1 = instance._files_checksum(['file1'])
+        assert isinstance(checksum_1, string_types)
+        assert checksum_1.isalnum()
+
+        checksum_2 = instance._files_checksum(['file2'])
+        assert checksum_2.isalnum()
+        assert checksum_1 != checksum_2
+
+        checksum_1_11 = instance._files_checksum(['file1', 'dir1/file11'])
+        assert checksum_1_11.isalnum()
+        assert checksum_1_11 != checksum_1
+        assert checksum_1_11 != checksum_2
+
+        checksum_11_1 = instance._files_checksum(['dir1/file11', 'file1'])
+        assert checksum_1_11 == checksum_11_1
+
+        checksum_1_11_1 = instance._files_checksum(
+            ['file1', 'dir1/file11', 'file1']
+        )
+        assert checksum_1_11 == checksum_1_11_1
+
+        (repo / 'file1').write('New content')
+        (repo / 'file2').write('New content')
+        checksum_1_c = instance._files_checksum(['file1'])
+        checksum_2_c = instance._files_checksum(['file2'])
+        assert checksum_1_c != checksum_1
+        assert checksum_2_c != checksum_2
+        assert checksum_1_c != checksum_2_c
+
+        (repo / 'file1').write('New content')
+        checksum_1_c2 = instance._files_checksum(['file1'])
+        assert checksum_1_c2 == checksum_1_c
