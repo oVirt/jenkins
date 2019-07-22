73ed6edf23cbeffd33063e44be8b7bc8afb35bd8
Barak Korren <bkorren@redhat.com>
2019-07-22 17:17:13 +0300

    Add Zuul jobs & project templates for gating
    Most code for the Zuul jobs themselves had been copied from the staging
    system where it was already tested.
    
    Also add apparatus for testing gating logic by having gating for this
    project (`jenkins`):
    
    - `release-branches` definition had been added to this project to make
      it ship artifacts into the 'dummy' change queue and/or via the
      appropriate gate.
    - As a result of the above, the 'dummy' change queue had been added to
      prevent errors that result from it not existing.
    - A stub test suit had been added to this project to enable it to
      function as a system testing project.
    - Gating Jenkins and Zuul jobs had been created to gate patches for
      this project by using the test suit that was added as described above.
    
    Change-Id: I84aeb8794128f86ca41bf9678cffc0726010f9d7
    Signed-off-by: Barak Korren <bkorren@redhat.com>

---
+++ automation/dummy_suite_master.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+# dummy_suit_master.sh - A dummy system tests suit that does nothing ATM
+#
+echo "This is a dummy system tests suit that does nothing"
+echo
+echo "Dump of provided extra_sources follows:"
+echo "---------------------------------------"
+cat extra_sources
+echo "---------------------------------------"
+echo
+echo "All done."
