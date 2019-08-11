989d8518448ff1f2059cc85e723111bfe369d4a9
Barak Korren <bkorren@redhat.com>
2019-08-11 10:47:22 +0300

    dummy_suite_master: Check for gated repo
    Add a check to the `dummy_suite_master` gating test suit for the
    insertion of the `gated` repo into `*.yumrepos` files.
    
    This patch requires the following patch to be merged or applied with JJB,
    and for the resulting job to be invoked for the relevant repo to exist
    in the 1st place:
    
        https://gerrit.ovirt.org/102553
    
    This patch also requires that the patch its based on will be merged for
    the gate tests to pass, because merging will cause a package to land in
    the gated repo ensuring that it is not empty.
    
    Change-Id: I686159c94e83311b2aaaf26ed2286c28ac0c672f
    Signed-off-by: Barak Korren <bkorren@redhat.com>

--- automation/dummy_suite_master.sh
+++ automation/dummy_suite_master.sh
@@ -1,11 +1,40 @@
 #!/bin/bash -e
 # dummy_suit_master.sh - A dummy system tests suit that does nothing ATM
 #
-echo "This is a dummy system tests suit that does nothing"
-echo
-echo "Dump of provided extra_sources follows:"
-echo "---------------------------------------"
-cat extra_sources
-echo "---------------------------------------"
-echo
-echo "All done."
+main() {
+    echo "This is a dummy system tests suit that does nothing"
+    echo
+    show_extra_sources
+    check_gated_gepo
+    echo "All done."
+}
+
+show_extra_sources() {
+    echo "Dump of provided extra_sources follows:"
+    echo "---------------------------------------"
+    cat extra_sources
+    echo "---------------------------------------"
+    echo
+}
+
+check_gated_gepo() {
+    echo "Dump of suit repos file follows:"
+    echo "--------------------------------"
+    cat automation/dummy_suite_master.yumrepos
+    echo "--------------------------------"
+    echo
+    if
+        yum -c automation/dummy_suite_master.yumrepos repolist -q \
+        | grep gated-dummy-master-el7
+    then
+        echo "---> Found gated repo in configuration"
+        return 0
+    else
+        echo "---> Gated repo missing from configuration"
+        return 1
+    fi
+}
+
+if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
+    main "$@"
+fi
