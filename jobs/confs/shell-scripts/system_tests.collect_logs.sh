ced660c83355d34c96ab58d6aaf2a9b50ef773e5
Galit <grosenth@redhat.com>
2021-05-11 10:47:10 +0300

    OST isn't using reposync files, no need to collect it
    And the reposync files are in a remove process.
    
    Change-Id: I2a7fc5c3f7fcc82572f111a361fd9df79b75ff8e
    Signed-off-by: Galit <grosenth@redhat.com>

--- jobs/confs/shell-scripts/system_tests.collect_logs.sh
+++ jobs/confs/shell-scripts/system_tests.collect_logs.sh
@@ -19,10 +19,6 @@ if [[ -d "$TESTS_LOGS" ]]; then
     mv "$TESTS_LOGS/"* "$WORKSPACE/exported-artifacts/"
 fi
 
-# export reposync-config.repo and extra_sources so we know the
-# repose we used
-suit_dir="$PROJECT/${{SUITE_TYPE}}-suite-${{VERSION}}"
-cp "$suit_dir"/*.repo exported-artifacts
 if [[ -f '$PROJECT/extra_sources' ]]; then
     cp $PROJECT/extra_sources exported-artifacts
 fi
